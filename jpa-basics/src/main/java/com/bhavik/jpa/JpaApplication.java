53f277b09239cc47eb6bd9d958ca1185dec072e5
bhavik <bhavikvashi1804@gmail.com>
2024-03-31 15:18:05 +0530

    Project 1: JPA Basics : One-To-One Relationship & Lazy Loading Demo

--- jpa-basics/src/main/java/com/bhavik/jpa/JpaApplication.java
+++ jpa-basics/src/main/java/com/bhavik/jpa/JpaApplication.java
@@ -1,13 +1,27 @@
 package com.bhavik.jpa;
 
+import com.bhavik.jpa.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaApplication {
+public class JpaApplication implements CommandLineRunner {
+
+	@PersistenceContext
+	EntityManager entityManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+		Student student = entityManager.find(Student.class, 1L);
+		System.out.println(student);
+	}
 }
